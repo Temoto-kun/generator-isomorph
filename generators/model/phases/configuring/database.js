8dd26ece6872be8d893df5cbc7079180a7afd160
Temoto-kun <kiiroifuriku@hotmail.com>
2016-04-10 16:40:21 +0800

    Refactor code of writing to database

--- generators/model/phases/configuring/database.js
+++ generators/model/phases/configuring/database.js
@@ -1,5 +1,5 @@
 (function () {
-    var pluralize;
+    var pluralize, table;
 
     pluralize = require('pluralize');
 
@@ -24,89 +24,108 @@
             debug: true
         });
         modelName = pluralize(model.name).toLowerCase().replace(/\s+/g, '_');
-        db.schema.createTable(modelName, function (table) {
+        db.schema.createTable(modelName, function (tableInst) {
+            table = tableInst;
+            
             table.increments();
 
             model.attrs.forEach(function (attr) {
-                var column;
-
-                switch (attr.typeBase) {
-                    case 'boolean':
-                        column = table.boolean(attr.name);
-                        break;
-                    case 'integer':
-                        switch (attr.type) {
-                            case 'BIGINT':
-                                column = table.bigInteger(attr.name);
-                                break;
-                            default:
-                                column = table.integer(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'string':
-                        switch (attr.type) {
-                            case 'TEXT':
-                                column = table.text(attr.name);
-                                break;
-                            case 'MEDIUMTEXT':
-                                column = table.text(attr.name, 'mediumtext');
-                                break;
-                            case 'LONGTEXT':
-                                column = table.text(attr.name, 'longtext');
-                                break;
-                            case 'VARCHAR':
-                            case 'CHARACTER':
-                                column = table.string(attr.name, attr.length);
-                                break;
-                        }
-                        break;
-                    case 'float':
-                        switch (attr.type) {
-                            case 'DECIMAL':
-                                column = table.decimal(attr.name);
-                                break;
-                            default:
-                                column = table.float(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'date':
-                        switch (attr.type) {
-                            case 'DATE':
-                                column = table.date(attr.name);
-                                break;
-                            case 'TIME':
-                                column = table.time(attr.name);
-                                break;
-                            case 'DATETIME':
-                                column = table.dateTime(attr.name);
-                                break;
-                            case 'TIMESTAMP':
-                                column = table.timestamp(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'file':
-                        switch (attr.type) {
-                            case 'VARCHAR':
-                                column = table.text(attr.name, 255);
-                                break;
-                            case 'BLOB':
-                                column = table.binary(attr.name);
-                                break;
-                        }
-                        break;
-                }
-                if (attr.characteristics.nullable) {
-                    column = column.notNullable();
-                }
+                configureSqlColumn(attr);
             });
         })
             .then(function () {
                 db.destroy();
                 return cb(null);
             });
+    }
+    
+    function configureSqlColumn(attr) {
+        var column;
+        
+        switch (attr.typeBase) {
+            case 'boolean':
+                column = table.boolean(attr.name);
+                break;
+            case 'integer':
+                switch (attr.type) {
+                    case 'BIGINT':
+                        column = table.bigInteger(attr.name);
+                        break;
+                    case 'INTEGER':
+                        column = table.integer(attr.name);
+                        break;
+                    default:
+                        column = table.specificType(attr.name, attr.type);
+                        break;
+                }
+                break;
+            case 'string':
+                switch (attr.type) {
+                    case 'TEXT':
+                        column = table.text(attr.name);
+                        break;
+                    case 'MEDIUMTEXT':
+                        column = table.text(attr.name, 'mediumtext');
+                        break;
+                    case 'LONGTEXT':
+                        column = table.text(attr.name, 'longtext');
+                        break;
+                    case 'VARCHAR':
+                        column = table.string(attr.name, attr.length);
+                        break;
+                    default:
+                        column = table.specificType(attr.name, attr.type);
+                        break;
+                }
+                break;
+            case 'float':
+                switch (attr.type) {
+                    case 'DECIMAL':
+                        column = table.decimal(attr.name);
+                        break;
+                    case 'FLOAT':
+                        column = table.float(attr.name);
+                        break;
+                    default:
+                        column = table.specificType(attr.name, attr.type);
+                        break;
+                }
+                break;
+            case 'date':
+                switch (attr.type) {
+                    case 'DATE':
+                        column = table.date(attr.name);
+                        break;
+                    case 'TIME':
+                        column = table.time(attr.name);
+                        break;
+                    case 'DATETIME':
+                        column = table.dateTime(attr.name);
+                        break;
+                    case 'TIMESTAMP':
+                        column = table.timestamp(attr.name);
+                        break;
+                    default:
+                        column = table.specificType(attr.name, attr.type);
+                        break;
+                }
+                break;
+            case 'file':
+                switch (attr.type) {
+                    case 'VARCHAR':
+                        column = table.text(attr.name, 255);
+                        break;
+                    case 'BLOB':
+                        column = table.binary(attr.name);
+                        break;
+                }
+                break;
+        }
+        if (attr.characteristics.nullable) {
+            column = column.notNullable();
+        }
+        
+        return column;
     }
 
     /**
@@ -127,71 +146,13 @@
             connection: require(currentDb)
         });
         modelName = model.name.toLowerCase().replace(/\s+/g, '_');
-        db.schema.createTable(modelName, function (table) {
+        db.schema.createTable(modelName, function (tableInst) {
+            table = tableInst;
+            
             table.increments();
 
             model.attrs.forEach(function (attr) {
-                switch (attr.typeBase) {
-                    case 'boolean':
-                        table.boolean(attr.name);
-                        break;
-                    case 'integer':
-                        switch (attr.type) {
-                            case 'BIGINT':
-                                table.bigInteger(attr.name);
-                                break;
-                            default:
-                                table.integer(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'string':
-                        switch (attr.type) {
-                            case 'TEXT':
-                                table.text(attr.name);
-                                break;
-                            case 'MEDIUMTEXT':
-                                table.text(attr.name, 'mediumtext');
-                                break;
-                            case 'LONGTEXT':
-                                table.text(attr.name, 'longtext');
-                                break;
-                            case 'VARCHAR':
-                            case 'CHARACTER':
-                                table.string(attr.name, attr.length);
-                                break;
-                        }
-                        break;
-                    case 'float':
-                        switch (attr.type) {
-                            case 'DECIMAL':
-                                table.decimal(attr.name);
-                                break;
-                            default:
-                                table.float(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'date':
-                        switch (attr.type) {
-                            case 'DATE':
-                                table.date(attr.name);
-                                break;
-                            case 'TIME':
-                                table.time(attr.name);
-                                break;
-                            case 'DATETIME':
-                                table.dateTime(attr.name);
-                                break;
-                            case 'TIMESTAMP':
-                                table.timestamp(attr.name);
-                                break;
-                        }
-                        break;
-                    case 'binary':
-                        table.binary(attr.name);
-                        break;
-                }
+                configureSqlColumn(attr);
             });
         })
             .then(function () {
